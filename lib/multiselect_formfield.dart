cf45552427d8547252cfee1bf537cacfa2a0ad83
Skyler Thompson <skythomp16@outlook.com>
2022-12-07 22:00:18 -0600

    Update multiselect_formfield.dart

--- lib/multiselect_formfield.dart
+++ lib/multiselect_formfield.dart
@@ -1,7 +1,7 @@
 library multiselect_formfield;
 
 import 'package:flutter/material.dart';
-import 'package:bible_reading_plan_generator/multiselect_dialog.dart';
+import 'package:multiselect_formfield/multiselect_dialog.dart';
 
 class MultiSelectFormField extends FormField<dynamic> {
   final Widget title;
